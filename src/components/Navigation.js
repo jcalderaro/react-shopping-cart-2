29b6a90ae74a8addcb142b24a9cf706e701d41a4
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-05 09:11:23 -0800

    MVP-Components

---
+++ src/components/Navigation.js
@@ -0,0 +1,20 @@
+/* -------------------------------------------------------------- */
+import React, { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import {CartContext} from '../contexts/CartContext.js';
+/* -------------------------------------------------------------- */
+const Navigation = () => {
+	const cart = useContext(CartContext);
+	console.log('navigation: ', cart);
+	return (
+		<div className="navigation">
+			<NavLink to="/">Products</NavLink>
+			<NavLink to="/cart">
+				Cart <span>{cart.length}</span>
+			</NavLink>
+		</div>
+	);
+};
+/* -------------------------------------------------------------- */
+export default Navigation;
+/* -------------------------------------------------------------- */
