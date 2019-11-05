6f4fde17966bde71856324781a59b1b1b6cdf0f0
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-11-05 09:07:06 -0800

    MVP-App.js

---
+++ src/App.js
@@ -0,0 +1,38 @@
+/* -------------------------------------------------------------- */
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
+import data from './data';
+import {ProductContext} from './contexts/ProductContext.js';
+import {CartContext} from './contexts/CartContext.js';
+/* -------------------------------------------------------------- */
+import Navigation from './components/Navigation';
+import Products from './components/Products';
+import ShoppingCart from './components/ShoppingCart';
+/* -------------------------------------------------------------- */
+function App() {
+	const [products] = useState(data);
+	const [cart, setCart] = useState([]);
+	const addItem = item => {
+		setCart([...cart, item]);
+	};
+	return (
+		<div className="App">
+			<ProductContext.Provider value={{products, addItem}}>
+			<CartContext.Provider value={cart}>
+				<Navigation cart={cart} />	
+				<Route
+					exact
+					path="/"
+					component={Products}/>
+				<Route
+					path="/cart"
+					render={() => <ShoppingCart cart={cart} />}
+				/>
+			</CartContext.Provider>
+			</ProductContext.Provider>
+		</div>
+	);
+};
+/* -------------------------------------------------------------- */
+export default App;
+/* -------------------------------------------------------------- */
